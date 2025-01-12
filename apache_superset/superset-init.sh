880067d7ff6ef3b0acf7e1fb2885fd9a5543b002
Aysr01 <ayoub.sarab@ine.inpt.ac.ma>
2025-01-12 15:05:00 +0100

    Integrate Apache superset to the project

---
+++ apache_superset/superset-init.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Create db and role if not exists
+python3 /app/create_db.py
+# create Admin user, you can read these values from env or anywhere else possible
+superset fab create-admin --username "$ADMIN_USERNAME" --firstname Superset --lastname Admin --email "$ADMIN_EMAIL" --password "$ADMIN_PASSWORD"
+
+# Upgrading Superset metastore
+superset db upgrade
+
+# setup roles and permissions
+superset superset init 
+
+# Starting server
+/bin/sh -c /usr/bin/run-server.sh
