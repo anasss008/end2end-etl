f7fb85148c8ae0f01bb2c68bf61f93c05cef3800
Aysr01 <ayoub.sarab@ine.inpt.ac.ma>
2025-01-13 20:07:57 +0100

    Correct superset db creation

--- apache_superset/create_db.py
+++ apache_superset/create_db.py
@@ -1,4 +1,5 @@
 import psycopg2
+import time
 
 # Define connection parameters
 admin_conn_info = "host=postgres dbname=airflow user=airflow password=airflow"
@@ -6,19 +7,29 @@ role_name = "superset"
 db_name = "superset"
 role_password = "superset"
 
-# Create the database
-conn = psycopg2.connect(admin_conn_info)
-conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
-cur = conn.cursor()
-cur.execute(f"CREATE DATABASE {db_name};")
-cur.close()
-conn.close()
 
-# Then connect again to create user and grant privileges
-with psycopg2.connect(admin_conn_info) as conn:
-    conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
-    with conn.cursor() as cur:
-        cur.execute(f"CREATE USER {role_name} WITH PASSWORD '{role_password}';")
-        cur.execute(f"GRANT ALL PRIVILEGES ON DATABASE {db_name} TO {role_name};")
+def create_db_role():
+    # Create the database
+    try:
+        conn = psycopg2.connect(admin_conn_info)
+        conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+        cur = conn.cursor()
+        cur.execute(f"CREATE DATABASE {db_name};")
+        cur.close()
+        conn.close()
 
+        # Then connect again to create user and grant privileges
+        with psycopg2.connect(admin_conn_info) as conn:
+            conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+            with conn.cursor() as cur:
+                cur.execute(f"CREATE USER {role_name} WITH PASSWORD '{role_password}';")
+                cur.execute(f"GRANT ALL PRIVILEGES ON DATABASE {db_name} TO {role_name};")
+    
+    except:
+        print("retry in 1min")
+        time.sleep(60)
+        create_db_role()
+
+
+create_db_role()
 print("DB created successfully!")
